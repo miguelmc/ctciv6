1d40536400a18f63379678883013d0ecc6a84665
miguelmc <miguelmtz93@gmail.com>
2015-10-14 05:32:17 -0400

    More headers to template, gitignore now ignores .txts

--- template.cpp
+++ template.cpp
@@ -1,18 +1,20 @@
 #include <algorithm>
+#include <cassert>
 #include <cctype>
 #include <cmath>
 #include <cstring>
 #include <iostream>
 #include <limits>
+#include <list>
 #include <map>
 #include <set>
-
+#include <stdio.h>
 #include <stdlib.h>
 
 using namespace std;
 
-int main() {
 
+int main() {
 
   return 0;
 }
